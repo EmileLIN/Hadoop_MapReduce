961657bc1ba6c4f23308f2a059b6ec9f4f6c2d97
EmileLIN <linshuhan52525@gmail.com>
2015-07-05 16:20:53 +0200

    Post And Answer Mapper
    The mapper to study correlation between posts’ lengths and the mean
    length of its answers

--- Intro to Hadoop and MapReduce/Final_Project/Post_And_Answer/mapper.py
+++ Intro to Hadoop and MapReduce/Final_Project/Post_And_Answer/mapper.py
@@ -14,7 +14,8 @@ for line in reader:
 	if (line != None) and (len(line) == 19):
 		node_id, title, tagnames, author_id, body, node_type, parent_id, abs_parent_id, added_at, score, state_string, last_edited_id, last_activity_by_id, last_activity_at, active_version_id, extra, extra_ref_id, extra_count, marked = line 
 		hour = (added_at.strip().split(" ",1)[-1]).split(":")[0]
-		new_data = [author_id,hour]
+		bodylength = len(body)
+		new_data = [node_id,node_type,parent_id,bodylength]
 	
 		writer.writerow(new_data)
 		
